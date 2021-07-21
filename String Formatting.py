8aafde6acb9d2b7cd5d04e0d017fe687d7822f2a
amadeusgrim <acquahj45@gmail.com>
2021-07-21 19:02:35 +0000

    string formatting and manipulation code and notes

---
+++ String Formatting.py
@@ -0,0 +1,35 @@
+a = "Sugondese"
+
+print(a[2])
+
+print(a[2:5])
+
+#STring interpolation 
+
+print(f"{a} These nuts")
+
+#Striing Formatting
+name = "Micah Nii"
+age = "28"
+Date_of_birth = "5/7/1992"
+Marital_Status= "false"
+Gender = "Male"
+Height = "1.65"
+Children = ["Alpha", "David", "Geofrey"]
+
+print("{0} is a {1} of {2} years old ".format(name,Gender, age))
+
+#Name index 
+mes = " i have a very nice {house}".format(house = "mansion")
+
+print(mes)
+
+#String COncatunation 
+
+a1 = "Sugon"
+a2 = "dese"
+a3 = "nuts"
+
+full = a1 + a2 + " " + a3
+print(full)
+
